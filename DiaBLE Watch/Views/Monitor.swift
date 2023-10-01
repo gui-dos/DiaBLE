039a5eaf85e24355cd3a957e54bfaa7993c30d48
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2023-10-01 15:52:12 +0200

    watchOS 10: embed Monitor in a ScrollView again

--- DiaBLE Watch/Views/Monitor.swift
+++ DiaBLE Watch/Views/Monitor.swift
@@ -20,168 +20,169 @@ struct Monitor: View {
 
     var body: some View {
 
-        // ScrollView {  // watchOS 9
-
         NavigationView {
 
-            VStack(spacing: 0) {
+            ScrollView {
 
                 VStack(spacing: 0) {
 
-                    HStack {
+                    VStack(spacing: 0) {
 
-                        VStack(spacing: 0) {
-                            if app.lastReadingDate != Date.distantPast {
-                                Text(app.lastReadingDate.shortTime).monospacedDigit()
-                                Text("\(minutesSinceLastReading) min ago").font(.system(size: 10)).monospacedDigit().lineLimit(1)
-                                    .onReceive(minuteTimer) { _ in
-                                        minutesSinceLastReading = Int(Date().timeIntervalSince(app.lastReadingDate)/60)
-                                    }
-                            } else {
-                                Text("---")
-                            }
-                        }
-                        .font(.footnote).frame(maxWidth: .infinity, alignment: .trailing ).foregroundColor(Color(.lightGray))
-                        .onReceive(app.$lastReadingDate) { readingDate in
-                            minutesSinceLastReading = Int(Date().timeIntervalSince(readingDate)/60)
-                        }
+                        HStack {
 
-                        Text(app.currentGlucose > 0 ? "\(app.currentGlucose.units)" : "---")
-                            .font(.system(size: 26, weight: .black)).monospacedDigit()
-                        // avoid truncation in 40 mm models
-                            .scaledToFill()
-                            .minimumScaleFactor(0.85)
-                            .foregroundColor(.black)
-                            .padding(.vertical, 0).padding(.horizontal, 4)
-                            .background(app.currentGlucose > 0 && (app.currentGlucose > Int(settings.alarmHigh) || app.currentGlucose < Int(settings.alarmLow)) ?
-                                        Color.red : Color.blue)
-                            .cornerRadius(6)
-
-                        // TODO: display both delta and trend arrow
-                        Group {
-                            if app.trendDeltaMinutes > 0 {
-                                VStack(spacing: -6) {
-                                    Text("\(app.trendDelta > 0 ? "+ " : app.trendDelta < 0 ? "- " : "")\(app.trendDelta == 0 ? "→" : abs(app.trendDelta).units)")
-                                        .fontWeight(.black)
-                                        .fixedSize()
-                                    Text("\(app.trendDeltaMinutes)m").font(.footnote)
-                                }.frame(maxWidth: .infinity, alignment: .leading).padding(.leading, 10)
-                            } else {
-                                Text(app.oopTrend.symbol).font(.system(size: 28)).bold()
-                                    .frame(maxWidth: .infinity, alignment: .leading).padding(.leading, 10)
-                            }
-                        }.foregroundColor(app.currentGlucose > 0 && ((app.currentGlucose > Int(settings.alarmHigh) && app.trendDelta > 0) || (app.currentGlucose < Int(settings.alarmLow) && app.trendDelta < 0)) ?
-                            .red : .blue)
-
-                    }
-
-                    if app.oopAlarm.description.count + app.oopTrend.description.count != 0 {
-                        Text("\(app.oopAlarm.description.replacingOccurrences(of: "_", with: " "))\(app.oopAlarm.description != "" ? " - " : "")\(app.oopTrend.description.replacingOccurrences(of: "_", with: " "))")
-                            .font(.footnote).foregroundColor(.blue).lineLimit(1)
-                    }
-
-                    HStack {
-                        if !app.deviceState.isEmpty {
-                            Text(app.deviceState)
-                                .foregroundColor(app.deviceState == "Connected" ? .green : .red)
-                                .font(.footnote).fixedSize()
-                        }
-                        if !app.deviceState.isEmpty && app.deviceState != "Disconnected" {
-                            Text(readingCountdown > 0 || app.deviceState == "Reconnecting..." ?
-                                 "\(readingCountdown) s" : "")
-                            .fixedSize()
-                            .font(Font.footnote.monospacedDigit()).foregroundColor(.orange)
-                            .onReceive(timer) { _ in
-                                // workaround: watchOS fails converting the interval to an Int32
-                                if app.lastConnectionDate == Date.distantPast {
-                                    readingCountdown = 0
+                            VStack(spacing: 0) {
+                                if app.lastReadingDate != Date.distantPast {
+                                    Text(app.lastReadingDate.shortTime).monospacedDigit()
+                                    Text("\(minutesSinceLastReading) min ago").font(.system(size: 10)).monospacedDigit().lineLimit(1)
+                                        .onReceive(minuteTimer) { _ in
+                                            minutesSinceLastReading = Int(Date().timeIntervalSince(app.lastReadingDate)/60)
+                                        }
                                 } else {
-                                    readingCountdown = settings.readingInterval * 60 - Int(Date().timeIntervalSince(app.lastConnectionDate))
+                                    Text("---")
+                                }
+                            }
+                            .font(.footnote).frame(maxWidth: .infinity, alignment: .trailing ).foregroundColor(Color(.lightGray))
+                            .onReceive(app.$lastReadingDate) { readingDate in
+                                minutesSinceLastReading = Int(Date().timeIntervalSince(readingDate)/60)
+                            }
+
+                            Text(app.currentGlucose > 0 ? "\(app.currentGlucose.units)" : "---")
+                                .font(.system(size: 26, weight: .black)).monospacedDigit()
+                            // avoid truncation in 40 mm models
+                                .scaledToFill()
+                                .minimumScaleFactor(0.85)
+                                .foregroundColor(.black)
+                                .padding(.vertical, 0).padding(.horizontal, 4)
+                                .background(app.currentGlucose > 0 && (app.currentGlucose > Int(settings.alarmHigh) || app.currentGlucose < Int(settings.alarmLow)) ?
+                                            Color.red : Color.blue)
+                                .cornerRadius(6)
+
+                            // TODO: display both delta and trend arrow
+                            Group {
+                                if app.trendDeltaMinutes > 0 {
+                                    VStack(spacing: -6) {
+                                        Text("\(app.trendDelta > 0 ? "+ " : app.trendDelta < 0 ? "- " : "")\(app.trendDelta == 0 ? "→" : abs(app.trendDelta).units)")
+                                            .fontWeight(.black)
+                                            .fixedSize()
+                                        Text("\(app.trendDeltaMinutes)m").font(.footnote)
+                                    }.frame(maxWidth: .infinity, alignment: .leading).padding(.leading, 10)
+                                } else {
+                                    Text(app.oopTrend.symbol).font(.system(size: 28)).bold()
+                                        .frame(maxWidth: .infinity, alignment: .leading).padding(.leading, 10)
+                                }
+                            }.foregroundColor(app.currentGlucose > 0 && ((app.currentGlucose > Int(settings.alarmHigh) && app.trendDelta > 0) || (app.currentGlucose < Int(settings.alarmLow) && app.trendDelta < 0)) ?
+                                .red : .blue)
+
+                        }
+
+                        if app.oopAlarm.description.count + app.oopTrend.description.count != 0 {
+                            Text("\(app.oopAlarm.description.replacingOccurrences(of: "_", with: " "))\(app.oopAlarm.description != "" ? " - " : "")\(app.oopTrend.description.replacingOccurrences(of: "_", with: " "))")
+                                .font(.footnote).foregroundColor(.blue).lineLimit(1)
+                        }
+
+                        HStack {
+                            if !app.deviceState.isEmpty {
+                                Text(app.deviceState)
+                                    .foregroundColor(app.deviceState == "Connected" ? .green : .red)
+                                    .font(.footnote).fixedSize()
+                            }
+                            if !app.deviceState.isEmpty && app.deviceState != "Disconnected" {
+                                Text(readingCountdown > 0 || app.deviceState == "Reconnecting..." ?
+                                     "\(readingCountdown) s" : "")
+                                .fixedSize()
+                                .font(Font.footnote.monospacedDigit()).foregroundColor(.orange)
+                                .onReceive(timer) { _ in
+                                    // workaround: watchOS fails converting the interval to an Int32
+                                    if app.lastConnectionDate == Date.distantPast {
+                                        readingCountdown = 0
+                                    } else {
+                                        readingCountdown = settings.readingInterval * 60 - Int(Date().timeIntervalSince(app.lastConnectionDate))
+                                    }
                                 }
                             }
                         }
                     }
-                }
 
-                Graph().frame(width: 31 * 4 + 60, height: 80)
-                    .padding(.vertical, 3)
+                    Graph().frame(width: 31 * 4 + 60, height: 80)
+                        .padding(.vertical, 3)
 
-                HStack(spacing: 2) {
+                    HStack(spacing: 2) {
 
-                    if app.sensor != nil && (app.sensor.state != .unknown || app.sensor.serial != "") {
-                        VStack(spacing: -4) {
-                            Text(app.sensor.state.description)
-                                .foregroundColor(app.sensor.state == .active ? .green : .red)
+                        if app.sensor != nil && (app.sensor.state != .unknown || app.sensor.serial != "") {
+                            VStack(spacing: -4) {
+                                Text(app.sensor.state.description)
+                                    .foregroundColor(app.sensor.state == .active ? .green : .red)
 
-                            if app.sensor.age > 0 {
-                                Text(app.sensor.age.shortFormattedInterval)
-                            }
-                        }
-                    }
-
-                    if app.device != nil {
-                        VStack(spacing: -4) {
-                            if app.device.battery > -1 {
-                                let battery = app.device.battery
-                                HStack(spacing: 4) {
-                                    let ext = battery > 95 ? 100 :
-                                    battery > 65 ? 75 :
-                                    battery > 35 ? 50 :
-                                    battery > 10 ? 25 : 0
-                                    Image(systemName: "battery.\(ext)")
-                                    Text("\(app.device.battery)%")
+                                if app.sensor.age > 0 {
+                                    Text(app.sensor.age.shortFormattedInterval)
                                 }
-                                .foregroundColor(app.device.battery > 10 ? .green : .red)
-                            }
-                            if app.device.rssi != 0 {
-                                Text("RSSI: ").foregroundColor(Color(.lightGray)) +
-                                Text("\(app.device.rssi) dB")
                             }
                         }
-                    }
 
-                }.font(.footnote).foregroundColor(.yellow)
+                        if app.device != nil {
+                            VStack(spacing: -4) {
+                                if app.device.battery > -1 {
+                                    let battery = app.device.battery
+                                    HStack(spacing: 4) {
+                                        let ext = battery > 95 ? 100 :
+                                        battery > 65 ? 75 :
+                                        battery > 35 ? 50 :
+                                        battery > 10 ? 25 : 0
+                                        Image(systemName: "battery.\(ext)")
+                                        Text("\(app.device.battery)%")
+                                    }
+                                    .foregroundColor(app.device.battery > 10 ? .green : .red)
+                                }
+                                if app.device.rssi != 0 {
+                                    Text("RSSI: ").foregroundColor(Color(.lightGray)) +
+                                    Text("\(app.device.rssi) dB")
+                                }
+                            }
+                        }
 
-                HStack {
+                    }.font(.footnote).foregroundColor(.yellow)
 
-                    Spacer()
-                    Spacer()
+                    HStack {
 
-                    Button {
-                        app.main.rescan()
-                    } label: {
-                        Image(systemName: "arrow.clockwise.circle").resizable().frame(width: 16, height: 16).foregroundColor(.blue)
-                    }
-                    .frame(height: 16)
-
-                    if (app.status.hasPrefix("Scanning") || app.status.hasSuffix("retrying...")) && app.main.centralManager.state != .poweredOff {
                         Spacer()
+                        Spacer()
+
                         Button {
-                            app.main.centralManager.stopScan()
-                            app.main.status("Stopped scanning")
-                            app.main.log("Bluetooth: stopped scanning")
+                            app.main.rescan()
                         } label: {
-                            Image(systemName: "stop.circle").resizable().frame(width: 16, height: 16).foregroundColor(.red)
+                            Image(systemName: "arrow.clockwise.circle").resizable().frame(width: 16, height: 16).foregroundColor(.blue)
                         }
                         .frame(height: 16)
+
+                        if (app.status.hasPrefix("Scanning") || app.status.hasSuffix("retrying...")) && app.main.centralManager.state != .poweredOff {
+                            Spacer()
+                            Button {
+                                app.main.centralManager.stopScan()
+                                app.main.status("Stopped scanning")
+                                app.main.log("Bluetooth: stopped scanning")
+                            } label: {
+                                Image(systemName: "stop.circle").resizable().frame(width: 16, height: 16).foregroundColor(.red)
+                            }
+                            .frame(height: 16)
+                        }
+
+                        Spacer()
+
+                        NavigationLink(destination: Details()) {
+                            Image(systemName: "info.circle").resizable().frame(width: 16, height: 16).foregroundColor(.blue)
+                        }.frame(height: 16)
+
+                        Spacer()
+                        Spacer()
                     }
 
-                    Spacer()
+                    Text(app.status.hasPrefix("Scanning") ? app.status : app.status.replacingOccurrences(of: "\n", with: " "))
+                        .font(.footnote)
+                        .lineLimit(app.status.hasPrefix("Scanning") ? 3 : 1)
+                        .truncationMode(app.status.hasPrefix("Scanning") ?.tail : .head)
+                        .frame(maxWidth: .infinity)
 
-                    NavigationLink(destination: Details()) {
-                        Image(systemName: "info.circle").resizable().frame(width: 16, height: 16).foregroundColor(.blue)
-                    }.frame(height: 16)
-
-                    Spacer()
-                    Spacer()
                 }
-
-                Text(app.status.hasPrefix("Scanning") ? app.status : app.status.replacingOccurrences(of: "\n", with: " "))
-                    .font(.footnote)
-                    .lineLimit(app.status.hasPrefix("Scanning") ? 3 : 1)
-                    .truncationMode(app.status.hasPrefix("Scanning") ?.tail : .head)
-                    .frame(maxWidth: .infinity)
-
             }
         }
         .navigationTitle("Monitor")
