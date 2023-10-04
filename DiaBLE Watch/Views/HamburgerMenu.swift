4c3a8175e5108dfc754e52c18a1af181f711df7e
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2023-10-04 17:11:17 +0200

    Watch: About Credits

--- DiaBLE Watch/Views/HamburgerMenu.swift
+++ DiaBLE Watch/Views/HamburgerMenu.swift
@@ -8,6 +8,17 @@ struct HamburgerMenu: View {
     @EnvironmentObject var history: History
     @EnvironmentObject var settings: Settings
 
+    let credits = [
+        "@dabear": "https://github.com/dabear",
+        "@ivalkou": "https://github.com/ivalkou",
+        "@j-kaltes": "https://github.com/j-kaltes",
+        "LibreMonitor": "https://github.com/UPetersen/LibreMonitor/tree/Swift4",
+        "Loop": "https://github.com/LoopKit/Loop",
+        "Nightscout LibreLinkUp Uploader": "https://github.com/timoschlueter/nightscout-librelink-up",
+        "xDrip+": "https://github.com/NightscoutFoundation/xDrip",
+        "xDrip4iO5": "https://github.com/JohanDegraeve/xdripswift"
+    ]
+
     var body: some View {
 
         NavigationStack {
@@ -62,26 +73,68 @@ struct HamburgerMenu: View {
                         }
                     }
 
-                    // TODO: Help and About
-                    HStack(spacing: 10) {
-                        NavigationLink(destination: Monitor()) {
-                            VStack {
-                                Image(systemName: "questionmark.circle").resizable().frame(width: 40, height: 40).offset(y: 4)
-                                Text("Help").bold().foregroundColor(.blue)
-                            }.frame(maxWidth: .infinity)
-                        }
-                        NavigationLink(destination: Monitor()) {
-                            VStack {
-                                Image(systemName: "info.circle").resizable().frame(width: 40, height: 40).offset(y: 4)
-                                Text("About").bold().foregroundColor(.blue)
-                            }.frame(maxWidth: .infinity)
-                        }
-                    }
+                    // TODO: Help and About sheets
+                    // HStack(spacing: 10) {
+                    //     VStack {
+                    //         Image(systemName: "questionmark.circle").resizable().frame(width: 40, height: 40).offset(y: 4)
+                    //         Text("Help").bold().foregroundColor(.blue)
+                    //     }.frame(maxWidth: .infinity)
+                    //     VStack {
+                    //         Image(systemName: "info.circle").resizable().frame(width: 40, height: 40).offset(y: 4)
+                    //         Text("About").bold().foregroundColor(.blue)
+                    //     }.frame(maxWidth: .infinity)
+                    // }
                 }
                 .foregroundColor(.red)
-                .buttonStyle(.plain)
+
+                Spacer(minLength: 30)
+
+                VStack(spacing: 20) {
+                    VStack {
+                        // TODO: get AppIcon 1024x1024
+                        // Image("AppIcon").resizable().frame(width: 100, height: 100)
+                        // FIXME: crashes in TestFlight (not in Release scheme)
+                        if UIImage(named: "AppIcon") != nil {
+                            Image(uiImage: UIImage(named: "AppIcon")!).resizable().frame(width: 100, height: 100)
+                        }
+                        Link("https://github.com/gui-dos/DiaBLE",
+                             destination: URL(string: "https://github.com/gui-dos/DiaBLE")!)
+                        .foregroundColor(.blue)
+                    }
+
+                    VStack {
+                        Image(systemName: "envelope.fill")
+                        Link(Data(base64Encoded: "Z3VpZG8uc29yYW56aW9AZ21haWwuY29t")!.string,
+                             destination: URL(string: "mailto:\(Data(base64Encoded: "Z3VpZG8uc29yYW56aW9AZ21haWwuY29t")!.string)")!)
+                        .foregroundColor(.blue)
+                    }
+
+                    VStack {
+                        Image(systemName: "giftcard")
+                        Link(Data(base64Encoded: "aHR0cHM6Ly9wYXlwYWwubWUvZ3Vpc29y")!.string, destination: URL(string: Data(base64Encoded: "aHR0cHM6Ly9wYXlwYWwubWUvZ3Vpc29y")!.string)!)
+                            .foregroundColor(.blue)
+                    }
+
+                    VStack {
+                        Text("Credits:")
+                        ScrollView {
+                            ForEach(credits.sorted(by: <), id: \.key) { name, url in
+                                VStack {
+                                    Text(name)
+                                    Link((url), destination: URL(string: url)!)
+                                        .foregroundColor(.blue)
+                                }
+                                .font(.footnote)
+                                .padding(.vertical, 6)
+                            }
+                        }
+                    }
+
+                }
+                .foregroundColor(.white)
 
             }
+            .buttonStyle(.borderless)
             .navigationTitle("DiaBLE  \(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String)")
             .navigationBarTitleDisplayMode(.inline)
 
