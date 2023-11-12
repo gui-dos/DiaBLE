13d969fe1d3d40253d986f84945f8d860f4cde6b
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2023-11-12 21:45:56 +0100

    Add Xcode 15.2 Widget Extension target

---
+++ Widgets/WidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by Guido Soranzio on 12/11/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+        WidgetsLiveActivity()
+    }
+}
