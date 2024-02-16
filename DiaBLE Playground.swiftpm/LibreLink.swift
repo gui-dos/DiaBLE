23b504be6311ac7b5d668c073a4df7d7ac2f2d3e
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2024-02-16 13:22:12 +0100

    TestFlight build 89

--- DiaBLE Playground.swiftpm/LibreLink.swift
+++ DiaBLE Playground.swiftpm/LibreLink.swift
@@ -403,46 +403,47 @@ class LibreLinkUp: Logging {
                             history.append(lastGlucose)
                             log("LibreLinkUp: graph values: \(history.map { ($0.glucose.id, $0.glucose.value, $0.glucose.date.shortDateTime, $0.color) })")
 
-                            // TODO: https://api.libreview.io/glucoseHistory?numPeriods=5&period=90
-                            let period = 15
-                            let numPeriods = 2
-                            if let ticketDict = json["ticket"] as? [String: Any],
-                               let token = ticketDict["token"] as? String {
-                                log("LibreView: new token for glucoseHistory: \(token)")
-                                request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-                                request.url = URL(string: "https://api.libreview.io/glucoseHistory?numPeriods=\(numPeriods)&period=\(period)")!
-                                debugLog("LibreView: URL request: \(request.url!.absoluteString), authenticated headers: \(request.allHTTPHeaderFields!)")
-                                let (data, response) = try await URLSession.shared.data(for: request)
-                                debugLog("LibreView: response data: \(data.string.trimmingCharacters(in: .newlines)), status: \((response as! HTTPURLResponse).statusCode)")
-                                if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
-                                   // let status = json["status"] as? Int,
-                                   let data = json["data"] as? [String: Any] {
-                                    let lastUpload = data["lastUpload"] as! Int
-                                    let lastUploadDate = Date(timeIntervalSince1970: Double(lastUpload))
-                                    let lastUploadCGM = data["lastUploadCGM"] as! Int
-                                    let lastUploadCGMDate = Date(timeIntervalSince1970: Double(lastUploadCGM))
-                                    let lastUploadPro = data["lastUploadPro"] as! Int
-                                    let lastUploadProDate = Date(timeIntervalSince1970: Double(lastUploadPro))
-                                    let reminderSent = data["reminderSent"] as! Bool
-                                    let devices = data["devices"] as! [Int]
-                                    let periods = data["periods"] as! [[String: Any]]
-                                    debugLog("LibreView: last upload date: \(lastUploadDate.local), last upload CGM date: \(lastUploadCGMDate.local), last upload pro date: \(lastUploadProDate.local), reminder sent: \(reminderSent), devices: \(devices), periods: \(periods.count)")
-                                    var i = 0
-                                    for period in periods {
-                                        let dateEnd = period["dateEnd"] as! Int
-                                        let endDate = Date(timeIntervalSince1970: Double(dateEnd))
-                                        let dateStart = period["dateStart"] as! Int
-                                        let startDate = Date(timeIntervalSince1970: Double(dateStart))
-                                        let daysOfData = period["daysOfData"] as! Int
-                                        let data = period["data"] as! [String: Any]
-                                        let blocks = data["blocks"] as! [[[String: Any]]]
-                                        i += 1
-                                        debugLog("LibreView: period # \(i) of \(periods.count), start date: \(startDate.local), end date: \(endDate.local), days of data: \(daysOfData)")
-                                        var j = 0
-                                        for block in blocks {
-                                            j += 1
-                                            debugLog("LibreView: block # \(j) of period # \(i): \(block.count) percentiles times: \(block.map { $0["time"] as! Int })")
-
+                            // TODO: https://api-eu.libreview.io/glucoseHistory?from=1700092800&numPeriods=5&period=14
+                            if settings.userLevel >= .test {
+                                let period = 15
+                                let numPeriods = 2
+                                if let ticketDict = json["ticket"] as? [String: Any],
+                                   let token = ticketDict["token"] as? String {
+                                    log("LibreView: new token for glucoseHistory: \(token)")
+                                    request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+                                    request.url = URL(string: "https://api.libreview.io/glucoseHistory?numPeriods=\(numPeriods)&period=\(period)")!
+                                    debugLog("LibreView: URL request: \(request.url!.absoluteString), authenticated headers: \(request.allHTTPHeaderFields!)")
+                                    let (data, response) = try await URLSession.shared.data(for: request)
+                                    debugLog("LibreView: response data: \(data.string.trimmingCharacters(in: .newlines)), status: \((response as! HTTPURLResponse).statusCode)")
+                                    if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+                                       // let status = json["status"] as? Int,
+                                       let data = json["data"] as? [String: Any] {
+                                        let lastUpload = data["lastUpload"] as! Int
+                                        let lastUploadDate = Date(timeIntervalSince1970: Double(lastUpload))
+                                        let lastUploadCGM = data["lastUploadCGM"] as! Int
+                                        let lastUploadCGMDate = Date(timeIntervalSince1970: Double(lastUploadCGM))
+                                        let lastUploadPro = data["lastUploadPro"] as! Int
+                                        let lastUploadProDate = Date(timeIntervalSince1970: Double(lastUploadPro))
+                                        let reminderSent = data["reminderSent"] as! Bool
+                                        let devices = data["devices"] as! [Int]
+                                        let periods = data["periods"] as! [[String: Any]]
+                                        debugLog("LibreView: last upload date: \(lastUploadDate.local), last upload CGM date: \(lastUploadCGMDate.local), last upload pro date: \(lastUploadProDate.local), reminder sent: \(reminderSent), devices: \(devices), periods: \(periods.count)")
+                                        var i = 0
+                                        for period in periods {
+                                            let dateEnd = period["dateEnd"] as! Int
+                                            let endDate = Date(timeIntervalSince1970: Double(dateEnd))
+                                            let dateStart = period["dateStart"] as! Int
+                                            let startDate = Date(timeIntervalSince1970: Double(dateStart))
+                                            let daysOfData = period["daysOfData"] as! Int
+                                            let data = period["data"] as! [String: Any]
+                                            let blocks = data["blocks"] as! [[[String: Any]]]
+                                            i += 1
+                                            debugLog("LibreView: period # \(i) of \(periods.count), start date: \(startDate.local), end date: \(endDate.local), days of data: \(daysOfData)")
+                                            var j = 0
+                                            for block in blocks {
+                                                j += 1
+                                                debugLog("LibreView: block # \(j) of period # \(i): \(block.count) percentiles times: \(block.map { $0["time"] as! Int })")
+                                            }
                                         }
                                     }
                                 }
