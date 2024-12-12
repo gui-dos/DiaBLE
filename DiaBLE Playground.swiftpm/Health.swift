aa41cad34d235fd01a95819a0fb6541e30c83327
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2024-12-12 09:50:04 +0100

    Playground: throw HKError(.errorHealthDataUnavailable) on an iPhone

--- DiaBLE Playground.swiftpm/Health.swift
+++ DiaBLE Playground.swiftpm/Health.swift
@@ -45,6 +45,7 @@ class HealthKit: Logging {
         do {
             // FIXME: *** Terminating app due to uncaught exception 'NSInvalidArgumentException', reason: 'NSHealthUpdateUsageDescription must be set in the app's Info.plist in order to request write authorization for the following types: HKQuantityTypeIdentifierBloodGlucose, HKQuantityTypeIdentifierDietaryCarbohydrates, HKQuantityTypeIdentifierInsulinDelivery'
             // try await store?.requestAuthorization(toShare: dataTypes, read: dataTypes)
+            throw HKError(.errorHealthDataUnavailable)
         } catch {
             log("HealthKit: error while requesting authorization for \(DataType.allCases) quantity types: \(error.localizedDescription)")
         }
