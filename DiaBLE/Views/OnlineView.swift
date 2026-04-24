31b3288a44321da8059d9571dae8768ef7c6bcaa
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2026-04-24 10:00:42 +0200

    Show LLU 5 login screen only in test user mode

--- DiaBLE/Views/OnlineView.swift
+++ DiaBLE/Views/OnlineView.swift
@@ -360,10 +360,13 @@ struct OnlineView: View, LoggingView {
                                     .textSelection(.enabled)
                             }
 
-                            WebView(site: "https://librelinkup.libreview.io/login",
-                                    query: "app=llu&appVersion=5.0.0.1077&installationId=" + settings.libreLinkUpInstallationId + "&osType=iOS&osVersion=26.5&locale=en-US&country=" + settings.libreLinkUpCountry,
-                                    delegate: app.main.libreLinkUp)
-                            .frame(height: proxy.size.height * 0.60)
+                            if settings.userLevel >= .test {
+                                WebView(site: "https://librelinkup.libreview.io",
+                                        endpoint: "login",
+                                        query: "app=llu&appVersion=5.0.0.1077&installationId=" + settings.libreLinkUpInstallationId + "&osType=iOS&osVersion=26.5&locale=en-US&country=" + settings.libreLinkUpCountry,
+                                        delegate: app.main.libreLinkUp)
+                                .frame(height: proxy.size.height * 0.60)
+                            }
 
                         }
                         #if targetEnvironment(macCatalyst)
