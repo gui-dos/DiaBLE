dd5a5ce5a3043656a684dd03d1a0933e61f8430c
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2026-05-13 07:38:43 +0200

    Remove Realm

--- DiaBLE Playground.swiftpm/ConsoleTools.swift
+++ DiaBLE Playground.swiftpm/ConsoleTools.swift
@@ -3,14 +3,6 @@ import SwiftUI
 import TabularData
 
 
-#if os(macOS)
-import RealmSwift
-#endif
-
-
-// TODO: rename to Copilot when smarter :-)
-
-
 struct ShellView: View, LoggingView {
 
     @Environment(AppState.self) var app: AppState
@@ -22,11 +14,6 @@ struct ShellView: View, LoggingView {
     @State private var showingFileImporter = false
     @State private var libreviewCSV = ""
 
-    @State private var showingFolderImporter = false
-    @State private var tridentContainer = ""
-
-    @State private var showingRealmKeyPrompt = false
-    @AppStorage("tridentRealmKey") var tridentRealmKey = ""  // 128-char hex
 
     var body: some View {
 
@@ -99,142 +86,6 @@ struct ShellView: View, LoggingView {
                     }
                     .padding(4)
 
-                    HStack {
-
-                        TextField("Trident Container", text: $tridentContainer)
-                            .textFieldStyle(.roundedBorder)
-                            .truncationMode(.head)
-
-                        Button {
-                            showingFolderImporter = true
-                        } label: {
-                            Image(systemName: "folder.circle")
-                                .font(.system(size: 32))
-                        }
-                        .fileImporter(
-                            isPresented: $showingFolderImporter,
-                            allowedContentTypes: [.folder]  // .directory doesn't work
-                        ) { result in
-                            switch result {
-                            case .success(let directory):
-                                guard directory.startAccessingSecurityScopedResource() else { return }
-                                defer { directory.stopAccessingSecurityScopedResource() }
-                                tridentContainer = directory.path
-                                let fileManager = FileManager.default
-                                let containerDirs = try! fileManager.contentsOfDirectory(atPath: tridentContainer)
-                                log("ls \(tridentContainer)\n\(containerDirs)")
-
-                                for dir in containerDirs {
-
-                                    if dir == "Library" {
-                                        let libraryDirs = try! fileManager.contentsOfDirectory(atPath: "\(tridentContainer)/Library")
-                                        log("ls Library\n\(libraryDirs)")
-                                        for dir in libraryDirs {
-                                            if dir == "Preferences" {
-                                                let preferencesContents = try! fileManager.contentsOfDirectory(atPath: "\(tridentContainer)/Library/Preferences")
-                                                log("ls Preferences\n\(preferencesContents)")
-                                                for plist in preferencesContents {
-                                                    if plist.hasPrefix("com.abbott.libre3") {
-                                                        if let plistData = fileManager.contents(atPath: "\(tridentContainer)/Library/Preferences/\(plist)") {
-                                                            if let libre3Plist = try? PropertyListSerialization.propertyList(from: plistData, options: [], format: nil) as? [String: Any] {
-                                                                log("cat \(plist)\n\(libre3Plist)")
-                                                                let realmEncryptionKey = libre3Plist["RealmEncryptionKey"] as! [UInt8]
-                                                                let realmEncryptionKeyInt8 = realmEncryptionKey.map { Int8(bitPattern: $0) }
-                                                                log("realmEncryptionKey:\n\(realmEncryptionKey)\nas Int8 array:\n\(realmEncryptionKeyInt8)")
-
-                                                                // https://frdmtoplay.com/freeing-glucose-data-from-the-freestyle-libre-3/
-                                                                //
-                                                                // Assuming that `python3` is available after installing the Xcode Command Line Tools
-                                                                // and `Library/Android/sdk/platform-tools/` is in your $PATH after installing Android Studio:
-                                                                //
-                                                                // $ pip3 install frida-tools
-                                                                // $ adb root
-                                                                // $ adb push ~/Downloads/frida-server-16.1.4-android-arm64 /data/local/tmp/frida-server
-                                                                // $ adb shell  # sudo waydroid shell
-                                                                // $ su
-                                                                // # chmod 755 /data/local/tmp/frida-server
-                                                                // # /data/local/tmp/frida-server &
-                                                                //
-                                                                // $ frida -U "Libre 3"
-                                                                // Frida-> Java.perform(function(){}) // Seems necessary to use Java.use
-                                                                // Frida-> var crypto_lib_def = Java.use("com.adc.trident.app.frameworks.mobileservices.libre3.security.Libre3SKBCryptoLib")
-                                                                // Frida-> var crypto_lib = crypto_lib_def.$new()
-                                                                // Frida-> unwrapped = crypto_lib.unWrapDBEncryptionKey([<realmEncryptionKeyInt8>])
-                                                                //
-                                                                // swift repl
-                                                                // import Foundation
-                                                                // let unwrappedInt8: [Int8] = [<unwrapped>]
-                                                                // let unwrappedUInt8: [UInt8] = unwrappedInt8.map { UInt8(bitPattern: $0) }
-                                                                // print(Data(unwrappedUInt8).reduce("", { $0 + String(format: "%02x", $1) }))
-
-                                                                // TODO: parse rest of libre3Plist
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-
-                                    if dir == "Documents" {
-                                        let documentsFiles = try! fileManager.contentsOfDirectory(atPath: "\(tridentContainer)/Documents")
-                                        log("ls Documents\n\(documentsFiles)")
-
-                                        for file in documentsFiles {
-
-                                            #if os(macOS)
-                                            if file.hasSuffix(".realm") && !file.contains("backup") {
-                                                var realm: Realm
-                                                var config = Realm.Configuration.defaultConfiguration
-                                                config.fileURL = URL(filePath: "\(tridentContainer)/Documents/\(file)")
-                                                config.schemaVersion = 8  // as for RealmStudio 14
-                                                do {
-                                                    if !file.contains("decrypted") {
-                                                        config.encryptionKey = tridentRealmKey.count == 128 ? tridentRealmKey.bytes : Data(count: 64)
-                                                    } else {
-                                                        config.encryptionKey = nil
-                                                    }
-                                                    realm = try Realm(configuration: config)
-                                                    if !file.contains("decrypted") {
-                                                        debugLog("Realm: opened encrypted \(tridentContainer)/Documents/\(file) by using the key \(tridentRealmKey)")
-                                                    } else {
-                                                        debugLog("Realm: opened already decrypted \(tridentContainer)/Documents/\(file)")
-                                                    }
-                                                    let sensors = realm.objects(SensorEntity.self)
-                                                    log("Realm: sensors: \(sensors)")
-                                                    let appConfig = realm.objects(AppConfigEntity.self)
-                                                    // overcome limit of max 100 objects in a result description
-                                                    log(appConfig.reduce("Realm: app config:", { $0 + "\n" + $1.description }))
-                                                    let libre3WrappedKAuth = realm.object(ofType: AppConfigEntity.self, forPrimaryKey: "Libre3WrappedKAuth")!["_configValue"]!
-                                                    log("Realm: libre3WrappedKAuth: \(libre3WrappedKAuth)")
-                                                    // TODO
-                                                } catch {
-                                                    log("Realm: error: \(error.localizedDescription)")
-                                                    if file == "trident.realm" {
-                                                        showingRealmKeyPrompt = true
-                                                    }
-                                                }
-                                            }
-                                            #endif // os(macOS)
-
-                                            if file == "trident.json" {
-                                                if let tridentJson = fileManager.contents(atPath: "\(tridentContainer)/Documents/\(file)") {
-                                                    (app.sensor as? Libre3 ?? Libre3(main: app.main)).parseRealmFlattedJson(data: tridentJson)
-                                                }
-                                            }
-                                        }
-                                    }
-
-                                }
-
-
-                            case .failure(let error):
-                                log("\(error.localizedDescription)")
-                            }
-                        }
-
-                    }
-                    .padding(4)
                 }
 
                 CrcCalculator()
@@ -243,37 +94,6 @@ struct ShellView: View, LoggingView {
             }
         }
         .background(.thinMaterial, ignoresSafeAreaEdges: [])
-        .sheet(isPresented: $showingRealmKeyPrompt) {
-            VStack(spacing: 20) {
-                Text("The Realm might be encrypted").fontWeight(.bold)
-                Text("Either this is not a Realm file or it's encrypted.")
-                TextField("128-character hex-encoded encryption key", text: $tridentRealmKey, axis: .vertical)
-                    .textFieldStyle(.roundedBorder)
-                    .padding()
-                HStack {
-                    Spacer()
-
-                    Button {
-                        showingRealmKeyPrompt = false
-                    } label: {
-                        Text("Cancel")
-                    }
-
-                    Button {
-                        showingRealmKeyPrompt = false
-                        showingFolderImporter = true
-                    } label: {
-                        Label {
-                            Text("Try again").fontWeight(.bold)
-                        } icon: {
-                            Image(systemName: "folder.circle").font(.system(size: 20))
-                        }
-                    }
-                }
-                .buttonStyle(.bordered)
-                .padding()
-            }
-        }
         .toolbar {
             Button {
                 withAnimation { showingStack.toggle() }
--- README.md
+++ README.md
@@ -1,26 +1,13 @@
 <p align="center"><img src="./DiaBLE/Assets.xcassets/AppIcon.appiconset/Icon.png" width="25%" /> &nbsp; &nbsp; <img src="https://github.com/gui-dos/DiaBLE/assets/7220550/901ad341-edfb-426e-9617-6763cf377447" width="20.5%"/> &nbsp; &nbsp; <img src="https://github.com/gui-dos/DiaBLE/assets/7220550/4d84f138-5b31-4db3-a407-f85265a78e66" width="20.5%" /></p>
 <br><br>
 
-**ChangeLog:**
-
-* 1/10/2024 - [Build 119](https://github.com/gui-dos/DiaBLE/commit/619382a)
-  - Scan Lingo's `patchInfo` via NFC:<br><br><div align="center"><img src="https://github.com/user-attachments/assets/d4f921b9-73a7-4b3d-8015-48343cf7f078" width="33.3%"/> &nbsp; <img src="https://github.com/user-attachments/assets/a30dda70-be3f-47d8-955a-19cd7a9b0e47" width="33.3%"/></div><br><br>
-  - Detect Stelo's device name starting with `DX01xx`
-  - Display warning when LibreLinkUp needs to re-accept Terms of Use:<br><br><div align="center"><img src="https://github.com/user-attachments/assets/1e02cbf2-96eb-4d3c-ad90-90eb3b0540e8" width="33.3%"/> &nbsp; <img src="https://github.com/user-attachments/assets/10ccfd08-62a9-46d7-849b-3c45f8d51835" width="33.3%"/></div><br><br>
-  - Detail when a Libre is a Plus
-  - Clear Dexcom G7 backfill minutes in BLE Setup:<br><br><div align="center"><img src="https://github.com/user-attachments/assets/be3db3f1-8cab-4d2e-a1a7-a5f1eaaa0441" width="33.3%"/> &nbsp; <img src="https://github.com/user-attachments/assets/f0fe63f6-4504-43d8-970a-2589bace56ee" width="33.3%"/></div><br><br>
-  
-* 20/1/2024 - [Build 88](https://github.com/gui-dos/DiaBLE/commit/d1333f3)  
-  - Shell: import and dump LibreView CSV files by using TabularData:
-<p align="center"><img src="https://github.com/gui-dos/DiaBLE/assets/7220550/01050cf3-2f75-4034-8861-5e33475c972b" width="75%"/></p>
-
 ## Builds
 
 To build the project, you have to duplicate the file _DiaBLE.xcconfig_, rename the copy to ***DiaBLEOverride.xcconfig*** (the missing reference displayed by Xcode in red should then point to it) and edit it by commenting out the trailing lines after `// Comment out the following...` and replacing `##TEAM_ID##` with your Apple Team ID, so that the first line should read, for example, `DEVELOPMENT_TEAM = Z25SC9UDC8`.
 
 The NFC capabilities require a paid Apple Developer Program **annual membership**. If you won't [request](https://developer.apple.com/contact/request/notifications-critical-alerts-entitlement/) a *Critical Alert Notifications Entitlement*, you have to edit out the lines `<key>com.apple.developer.usernotifications.critical-alerts</key>` `<true/>` from the _.entitlements_ files in the folders _DiaBLE_ and _DiaBLE Watch_.
 
-A public beta of DiaBLE is availaBLE on **[TestFlight](https://testflight.apple.com/join/s4vTFYpC)**: I'll periodically expunge anonymous users I didn't invite or who didn't sponsor me through [PayPal](https://paypal.me/guisor) $-). If you own an iPad you can download the [zipped archive](https://github.com/gui-dos/DiaBLE/archive/refs/heads/main.zip) of this repository and just tap _DiaBLE Playground.swiftpm_ to test the corresponding features and even more past legacy ones.
+A public beta of DiaBLE is availaBLE on **[TestFlight](https://testflight.apple.com/join/s4vTFYpC)**: I'll periodically expunge anonymous users I didn't invite or who didn't sponsor me through [PayPal](https://paypal.me/guisor) $-). If you own an iPad you can download the [zipped archive](https://github.com/gui-dos/DiaBLE/archive/refs/heads/main.zip) of this repository and just tap _DiaBLE Playground.swiftpm_ to test the corresponding features.
 
 Currently I am targeting only the latest betas of Xcode and iOS and focusing on the new **Libre 3** and **Dexcom G7**. Please consider my personal project still just a **prototype** (I am not even managing correcty the execution in the background yet), even though most open-source apps (and even commercial ones) make use of my naive NFC and BLE classes which unveil technical details not found elsewhere... ;-)
 
@@ -29,8 +16,6 @@ Currently I am targeting only the latest betas of Xcode and iOS and focusing on 
   * the temperature-based calibration algorithm has been derived from the old LibreLink 2.3: it is known that the Vendor improves its algorithms at every new release, smoothing the historical values and projecting the trend ones into the future to compensate the interstitial delay but these further stages aren't understood yet; I never was convinced by the simple linear regression models that others apply on finger pricks;
   * activating the BLE streaming of data on a Libre 2 will break other apps' pairings and you will have to reinstall them to get their alarms back again; in Test mode it is possiBLE however to eavesdrop on the incoming data of multiple apps running side-by-side by just activating the notifications on the known BLE characteristics: the same technique is used to analyze the Libre 3 incoming traffic since the Core Bluetooth connections are reference-counted;
   * connecting directly to a Libre 2/3 and a Dexcom G7 from an Apple Watch is currently just a proof of concept that it is technically possiBLE: keeping the connection in the background will require additional work and AFAIK nobody else is capaBLE of doing the job... :-P
-  
-The Shell in the Console allows opening both encrypted and decrypted _trident.realm_ files from a backup of the Libre 3 app data (the Container folder extracted for example by using iMazing): see the nice technical post (mentioning me 😎) ["Liberating glucose data from the Freestyle Libre 3"](https://frdmtoplay.com/freeing-glucose-data-from-the-freestyle-libre-3/) (a rooted Android Virtual Machine like Waydroid or the default _Google APIs System Image_ in Android Studio is required to unwrap the Realm encryption key).
 
 ## TODOs
 
