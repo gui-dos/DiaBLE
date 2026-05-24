df2fcf2b602ebed517f3e831e05737edc0c5d2a3
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2026-05-24 09:51:34 +0200

    Embed poml88/libre3-auth-ios-sample-app@64cb115

---
+++ DiaBLE/Libre3/libre3-auth-ios-sample-app-main/libre3AndroidServer/libre3AndroidServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  libre3AndroidServerApp.swift
+//  libre3AndroidServer
+//
+//  Created by Peter Müller on 12.05.26.
+//
+
+import SwiftUI
+
+@main
+struct libre3AndroidServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
